e2c0beff8deb2650dd8c30946a18e447e442de59
jagadishbisai02 <126754029+jagadishbisai02@users.noreply.github.com>
2023-10-24 17:37:02 +0530

    mongodb connected

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const DB ="mongodb+srv://jagadishbisai02:Jh@nsi789@cluster0.qwxh5ln.mongodb.net/mernstack?retryWrites=true&w=majority";
+const DB ="mongodb+srv://jagadishbisai02:Jh%40nsi789@cluster0.qwxh5ln.mongodb.net/mernstack?retryWrites=true&w=majority";
 
 //for connection to mongodbs
 mongoose.connect(DB, {
     useUnifiedTopology: true,
-    useNewUrlParse: true,
   })
   .then(() => console.log("database connected"))
   .catch((err) => {
